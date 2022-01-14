24cd2f906a81709206d9854ca99dd4cde8b5b024
Sumyeong-Jang <50583200+Sumyeong-Jang@users.noreply.github.com>
2022-01-14 13:54:15 +0900

    Create #11659_구간합구하기4_sample.cpp

---
+++ sds동적계획법1/#11659_구간합구하기4_sample.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int N, M;
+int input;
+int from, to;
+int dp[100001];
+int answer;
+
+int main(int argc, char** argv) {
+
+    scanf("%d", &N);
+    scanf("%d", &M);
+
+    for (int i = 1; i <= N; i++) {
+        scanf("%d", &input);
+        dp[i] += dp[i - 1] + input;
+    }
+
+    for (int i = 0; i < M; i++) {
+        scanf("%d", &from);
+        scanf("%d", &to);
+        answer = dp[to] - dp[from - 1];
+        printf("%d\n", answer);
+    }
+
+    return 0;
+}
