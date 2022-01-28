736863713b5a61808687d0f80d3ae032f5d8d36d
Sumyeong-Jang <50583200+Sumyeong-Jang@users.noreply.github.com>
2022-01-28 18:14:31 +0900

    Update #1103_게임.cpp

--- sds알고리즘 기초/#1103_게임.cpp
+++ sds알고리즘 기초/#1103_게임.cpp
@@ -1,62 +1,50 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
-#include <cstring>
-#include <cstdlib>
 #include <algorithm>
 
 using namespace std;
 
-int N, M, Ans;
+int N, M, ans;
 char board[51][51];
-bool visit[51][51];
-int dp[51][51]; // 중복계산을 막기 위해 계산 결과 저장
+bool isVisited[51][51];
+int DP[51][51]; //계산결과 저장해둠 (중복계산을 막기 위해)
 
-//방향 (왼쪽, 아래, 위, 오른쪽)
-int dx[] = { -1, 0, 0, 1 };
-int dy[] = { 0, -1, 1, 0 };
+int dx[] = { -1, 0, 1, 0 };
+int dy[] = { 0, 1, 0, -1 };
 
 bool dfs(int x, int y, int cnt);
 
-int main()
-{
+int main() {
 	scanf("%d %d", &N, &M);
-
 	for (int i = 0; i < N; i++)
 		scanf("%s", board[i]);
-
 	dfs(0, 0, 1);
-	printf("%d", Ans);
+	printf("%d", ans);
 }
 
-bool dfs(int x, int y, int cnt)
-{
+bool dfs(int x, int y, int cnt) {
 	// 무한번 움직일 수 있는 경우
-	if (Ans == -1)
-		return true;
+	if (ans == -1) return true;
+	ans = max(ans, cnt);
 
-	Ans = max(Ans, cnt);
-
-	visit[x][y] = true;
-	dp[x][y] = cnt;
+	isVisited[x][y] = true;
+	DP[x][y] = cnt;
 
 	for (int i = 0; i < 4; i++) {
 		int nx = x + dx[i] * (board[x][y] - '0');
 		int ny = y + dy[i] * (board[x][y] - '0');
 
-		if (nx < 0 || nx >= N || ny < 0 || ny >= M || board[nx][ny] == 'H')
-			continue;
+		if (nx < 0 || nx >= N || ny < 0 || ny >= M || board[nx][ny] == 'H') continue;
 
-		if (visit[nx][ny]) {
-			Ans = -1;
+		if (isVisited[nx][ny]) {
+			ans = -1;
 			return true;
 		}
-
-		if (dp[nx][ny] > 0 && cnt < dp[nx][ny])
-			continue;
+		if (DP[nx][ny] > 0 && cnt < DP[nx][ny]) continue;
 
 		if (dfs(nx, ny, cnt + 1))
 			return true;
 	}
-	visit[x][y] = false;
+	isVisited[x][y] = false;
 	return false;
 }
