9d878ee5cb849b2601c15529fc18792fd0e3c486
Sumyeong-Jang <50583200+Sumyeong-Jang@users.noreply.github.com>
2023-04-28 14:00:48 +0900

    [level 2] Title: 배달, Time: 0.14 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12978. 배달/배달.py
@@ -0,0 +1,40 @@
+import heapq
+
+def solution(N, road, K):
+    answer = 0
+
+    roadmap = [[] * N for _ in range(N + 1)]
+    """
+    for i in range(len(road)):
+        roadmap[road[i][0]][road[i][1]] = road[i][2]
+        roadmap[road[i][1]][road[i][0]] = road[i][2]
+    """
+    for r in road:
+        roadmap[r[0]].append([r[2], r[1]])
+        roadmap[r[1]].append([r[2], r[0]])
+    
+    dist = [float('inf')] * (N + 1)
+    dist[1] = 0
+    
+    dijkstra(roadmap, dist);
+    """
+    for d in dist:
+        print(d)
+        if d <= K:
+            ansewr = answer + 1
+    """
+    for i in range(1, len(dist)):
+        if dist[i] <= K:
+            answer = answer + 1
+    print(answer)
+    return answer
+
+def dijkstra(roadmap, dist):
+    heap = []
+    heapq.heappush(heap, [0, 1])
+    while heap:
+        cost, node = heapq.heappop(heap)
+        for c, n in roadmap[node]:
+            if cost + c < dist[n]:
+                dist[n] = cost + c
+                heapq.heappush(heap, [cost + c, n])
