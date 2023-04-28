ba346bee69cf88f2a53ca745a5cacdbdafcd8fa8
Sumyeong-Jang <50583200+Sumyeong-Jang@users.noreply.github.com>
2023-04-28 14:07:12 +0900

    [level 2] Title: 배달, Time: 0.08 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12978. 배달/배달.py
+++ 프로그래머스/lv2/12978. 배달/배달.py
@@ -4,11 +4,6 @@ def solution(N, road, K):
     answer = 0
 
     roadmap = [[] * N for _ in range(N + 1)]
-    """
-    for i in range(len(road)):
-        roadmap[road[i][0]][road[i][1]] = road[i][2]
-        roadmap[road[i][1]][road[i][0]] = road[i][2]
-    """
     for r in road:
         roadmap[r[0]].append([r[2], r[1]])
         roadmap[r[1]].append([r[2], r[0]])
@@ -21,12 +16,7 @@ def solution(N, road, K):
     for d in dist:
         if d <= K:
             answer = answer + 1
-    """
-    for i in range(1, len(dist)):
-        if dist[i] <= K:
-            answer = answer + 1
-    """
-    print(answer)
+    
     return answer
 
 def dijkstra(roadmap, dist):
