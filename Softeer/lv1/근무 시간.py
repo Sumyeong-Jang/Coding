8317167414f539f791be00c88c4b8f958f736f18
Sumyeong-Jang <50583200+Sumyeong-Jang@users.noreply.github.com>
2023-05-04 18:52:51 +0900

    [level 1] Title: 근무 시간, Time: 109 ms, Memory: 37.46 MB

--- Softeer/lv1/근무 시간.py
+++ Softeer/lv1/근무 시간.py
@@ -3,10 +3,8 @@ import sys
 m, h = 0, 0
 for _ in range(5):
     work_time = sys.stdin.readline().rstrip().split()
-    m_out = int(work_time[1][3]) * 10 + int(work_time[1][4])
-    h_out = int(work_time[1][0]) * 10 + int(work_time[1][1])
-    m_in = int(work_time[0][3]) * 10 + int(work_time[0][4])
-    h_in = int(work_time[0][0]) * 10 + int(work_time[0][1])
+    h_in, m_in = map(int, work_time[0].split(':'))
+    h_out, m_out = map(int, work_time[1].split(':'))
     
     if m_out < m_in:
         h_out -= 1
@@ -14,8 +12,5 @@ for _ in range(5):
     
     m += (m_out - m_in)
     h += (h_out - h_in)
-    if m >= 60:
-        h += 1
-        m -= 60
 
 print(h * 60 + m)
