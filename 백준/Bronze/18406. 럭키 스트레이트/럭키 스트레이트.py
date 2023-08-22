88cc0c652462c434a8c2797c1d81c1e77046389a
Sumyeong-Jang <50583200+Sumyeong-Jang@users.noreply.github.com>
2023-08-22 11:53:14 +0900

    [Bronze II] Title: 럭키 스트레이트, Time: 108 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Bronze/18406. 럭키 스트레이트/럭키 스트레이트.py
+++ 백준/Bronze/18406. 럭키 스트레이트/럭키 스트레이트.py
@@ -1,16 +1,18 @@
+import sys
+input = sys.stdin.readline
+
 N = int(input())
 num = []
 while N > 0:
     num.append(N % 10)
     N //= 10
-num.reverse()
 
-l, r = 0, 0
+left, right = 0, 0
 for i in range(len(num) // 2):
-    l += num[i]
-    r += num[len(num) - i - 1]
+    right += num[i]
+    left += num[len(num) - i - 1]
 
-if l == r:
-    print("LUCKY")
+if left == right:
+    print('LUCKY')
 else:
-    print("READY")
+    print('READY')
